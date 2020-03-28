df6b05a5da8f9e422afcc6377b1748049a3ed8ef
Justin <justinantojacob@gmail.com>
2020-03-28 10:48:54 +0400

    Phase 1 Begun.

---
+++ settings.py
@@ -0,0 +1,13 @@
+class Settings:
+    """A Class to store all settings for Alien Invasion."""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen Settings
+        self.screen_width = 1200
+        self.screen_height = 700
+        self.bg_color = (230, 230, 230)
+
+        # Ship settings
+        self.ship_speed = 1.5
+        
